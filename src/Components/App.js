7a2ac64be5cb59dac7ce3f9ec1a075592013169e
Ryan Canfield <ryancanfield@level11.com>
2016-09-08 10:16:29 -0700

    A more a11y friendly way to hide focus outlines.

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,9 +2,47 @@ import React, { Component } from 'react';
 
 export default class App extends Component {
 
+  getInitialState() {
+    return {
+      hideFocusOutline : true
+    };
+  },
+
+  componentDidMount() {
+    // calculate the responsive state after the component has been mounted
+    window.addEventListener('keydown', this.showFocusOutline);
+    window.addEventListener('mousemove', this.hideFocusOutline);
+  },
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.showFocusOutline);
+    window.removeEventListener('mousemove', this.hideFocusOutline);
+  },
+
+  showFocusOutline(event) {
+    if (this.state.hideFocusOutline && event.keyCode === 9) {
+      this.setState ({
+        hideFocusOutline : false
+      });
+    }
+  },
+
+  hideFocusOutline() {
+    if (!this.state.hideFocusOutline) {
+      this.setState ({
+        hideFocusOutline : true
+      });
+    }
+  },
+
   render () {
+    const classes = [
+      'app',
+      this.state.hideFocusOutline ? 'focus-outline--hidden' : null,
+    ].join(' ').trim();
+
     return (
-      <div className='app'>
+      <div className={classes}>
         <h1 className='app__greeting'>
           Example Application
         </h1>
